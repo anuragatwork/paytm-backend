4345c457ff11328d4aa4aa0837ad290638280429
anurag <121740153+anuragatwork@users.noreply.github.com>
2024-06-21 18:39:06 +0530

    first commit

---
+++ db.js
@@ -0,0 +1,55 @@
+// backend/db.js
+const mongoose = require('mongoose');
+const env=require('dotenv').config();
+const url=process.env.connectionUrl;
+mongoose.connect(url)
+
+// Create a Schema for Users
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+});
+
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+const Account = mongoose.model('Account', accountSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+	User,
+    Account
+};
---
+++ middleware.js
@@ -0,0 +1,28 @@
+
+const env=require('dotenv').config();
+const JWT_SECRET=process.env.JWT_SECRET;
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(403).json({});
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+
+        req.userId = decoded.userId;
+
+        next();
+    } catch (err) {
+        return res.status(403).json({msg:"error"});
+    }
+};
+
+module.exports = {
+    authMiddleware
+}
